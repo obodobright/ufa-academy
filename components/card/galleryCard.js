ea0c9595c128d64351f542fbff1c6052b127faf1
Obodo Bright <obodobright0@gmail.com>
2025-02-24 17:47:35 +0100

    UPDATE:MOBILE RESPONSIVENESS IS KEY

--- components/card/galleryCard.js
+++ components/card/galleryCard.js
@@ -2,8 +2,14 @@ import Image from "next/image";
 
 export const GalleryCard = ({ gallery }) => {
   return (
-    <div className="h-[250px] overflow-hidden rounded-md w-full  bg-gray-200">
-      <Image alt="gallery_img" className="w-full" src={gallery.img} width={250} height={250} />
+    <div className="h-[200px] md:h-[250px] overflow-hidden rounded-md w-full  bg-gray-200">
+      <Image
+        alt="gallery_img"
+        className="w-full h-full md:h-auto "
+        src={gallery.img}
+        width={250}
+        height={250}
+      />
     </div>
   );
 };
