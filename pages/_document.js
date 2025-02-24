39182871d6851f8b3e0512c4604dbc4831e76b79
Obodo Bright <obodobright0@gmail.com>
2025-02-24 17:31:08 +0100

    fix: UPDATE SEO STUFFS

--- pages/_document.js
+++ pages/_document.js
@@ -3,8 +3,46 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
-      <body className="antialiased">
+      <Head>
+        <meta name="title" content="Ugbogu FA - Football Academy In Delta" />
+        <meta
+          name="theme-color"
+          content="#F4B004
+"
+        />
+        <meta
+          name="description"
+          content="  At UBOGU Football Academy, our mission is to provide a world-class football
+              development program that nurtures the technical, tactical, physical, and mental growth
+              of young players. We are committed to creating a supportive and inclusive environment
+              that fosters a love for the game, promotes sportsmanship, and develops the next
+              generation of football leaders."
+        />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="http://www.theufaacademy.ng" />
+        <meta property="og:title" content="Ugbogu FA - Football Academy In Delta" />
+        <meta
+          property="og:description"
+          content="  At UBOGU Football Academy, our mission is to provide a world-class football
+              development program that nurtures the technical, tactical, physical, and mental growth
+              of young players. We are committed to creating a supportive and inclusive environment
+              that fosters a love for the game, promotes sportsmanship, and develops the next
+              generation of football leaders."
+        />
+        <meta property="og:image" content="http://www.theufaacademy.ng/favicon.ico" />
+
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content="https://x.com/uborafootball?s=11" />
+        <meta property="twitter:title" content="Ugbogu FA - Football Academy In Delta" />
+        <meta
+          property="twitter:description"
+          content="We are committed to creating a supportive and inclusive environment
+              that fosters a love for the game, promotes sportsmanship, and develops the next
+              generation of football leaders"
+        />
+        <meta property="twitter:image" content="http://www.theufaacademy.ng/favicon.ico" />
+      </Head>
+      <body>
         <Main />
         <NextScript />
       </body>
